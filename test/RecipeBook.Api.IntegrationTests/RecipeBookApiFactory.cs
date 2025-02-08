34cda69cedf114cdef89cd7a01f0acbcd78dc28a
Andres Hernandez <andhernand@gmail.com>
2025-02-08 16:46:40 -0700

    implement first integration test

---
+++ test/RecipeBook.Api.IntegrationTests/RecipeBookApiFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+using Testcontainers.PostgreSql;
+
+namespace RecipeBook.Api.IntegrationTests;
+
+public class RecipeBookApiFactory : WebApplicationFactory<IRecipeBookApiMarker>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _postgres = new PostgreSqlBuilder()
+        .WithImage("postgres:17.2")
+        .WithPassword("Sup3r!S3cr3t!T35ting")
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseSetting("ConnectionStrings:RecipeBook", _postgres.GetConnectionString());
+        base.ConfigureWebHost(builder);
+    }
+
+    public async ValueTask InitializeAsync()
+    {
+        await _postgres.StartAsync();
+    }
+}
---
+++ test/RecipeBook.Api.IntegrationTests/Recipes/GetAllRecipesTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+
+using RecipeBook.Api.Recipes;
+
+namespace RecipeBook.Api.IntegrationTests.Recipes;
+
+public class GetAllRecipesTests : IClassFixture<RecipeBookApiFactory>
+{
+    private readonly RecipeBookApiFactory _factory;
+
+    public GetAllRecipesTests(RecipeBookApiFactory factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task GetAllRecipes_WhenNoRecipesExist_ShouldReturnEmptyArray()
+    {
+        using var client = _factory.CreateClient();
+
+        var response = await client.GetAsync("/api/v1/recipes", TestContext.Current.CancellationToken);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        var content = await response.Content.ReadFromJsonAsync<Recipe[]>(TestContext.Current.CancellationToken);
+        content.ShouldBeEmpty();
+    }
+}
