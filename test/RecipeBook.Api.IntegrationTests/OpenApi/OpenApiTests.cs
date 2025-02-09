4d8a6a9a6c6a5c2e515496ea3529d4d89c6f1887
Andres Hernandez <andhernand@gmail.com>
2025-02-09 02:21:28 -0700

    implement openapi tests.

---
+++ test/RecipeBook.Api.IntegrationTests/OpenApi/OpenApiTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+
+namespace RecipeBook.Api.IntegrationTests.OpenApi;
+
+public class OpenApiTests(RecipeBookApiFactory factory) : TestBase(factory)
+{
+    [Fact]
+    public async Task OpenApi_WhenNavigatedTo_ShouldReturnOk()
+    {
+        using var client = CreateClient();
+
+        var response = await client.GetAsync("/openapi/v1.json", TestContext.Current.CancellationToken);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task Scalar_WhenNavigatedTo_ShouldReturnOk()
+    {
+        using var client = CreateClient();
+
+        var response = await client.GetAsync("/scalar", TestContext.Current.CancellationToken);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+    }
+}
