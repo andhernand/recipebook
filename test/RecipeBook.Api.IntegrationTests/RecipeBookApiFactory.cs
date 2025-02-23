b6c0fa5240e76f77048f74a11e4468772d606664
Andres Hernandez <andhernand@gmail.com>
2025-02-23 10:53:35 -0700

    implement cache into integration tests project.
    adds the testcontainers dependency
    adjust the WebApplicationFactory to also stand up a cache container.

--- test/RecipeBook.Api.IntegrationTests/RecipeBookApiFactory.cs
+++ test/RecipeBook.Api.IntegrationTests/RecipeBookApiFactory.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 
 using Testcontainers.PostgreSql;
+using Testcontainers.Redis;
 
 namespace RecipeBook.Api.IntegrationTests;
 
@@ -10,6 +11,10 @@ public class RecipeBookApiFactory : WebApplicationFactory<IRecipeBookApiMarker>,
     private readonly PostgreSqlContainer _postgres = new PostgreSqlBuilder()
         .WithImage("postgres:17.2")
         .WithPassword("Sup3r!S3cr3t!T35ting")
+        .Build();
+
+    private readonly RedisContainer _cache = new RedisBuilder()
+        .WithImage("docker.dragonflydb.io/dragonflydb/dragonfly")
         .Build();
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
@@ -21,11 +26,17 @@ public class RecipeBookApiFactory : WebApplicationFactory<IRecipeBookApiMarker>,
     public async ValueTask InitializeAsync()
     {
         await _postgres.StartAsync();
+        await _cache.StartAsync();
     }
 
     public override async ValueTask DisposeAsync()
     {
         await _postgres.StopAsync();
+        await _cache.StopAsync();
+
+        await _postgres.DisposeAsync();
+        await _cache.DisposeAsync();
+
         await base.DisposeAsync();
     }
 }
